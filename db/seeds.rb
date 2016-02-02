5eb97595f3507379ea0034f25d296b16cc792052
Enraz <enrazzz@gmail.com>
2016-02-02 23:29:49 +0100

    Adding posts to dt

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts("Creating Posts")
+puts("==============")
+
+post1 = Post.new(title: "Joker", gif: "http://45.media.tumblr.com/tumblr_m3h97gaBiq1rs9ie3o1_400.gif", votes: 8)
+post1.save
+post2 = Post.new(title: "Superman vs Batman", gif: "https://media.giphy.com/media/bYhKPg3nYWRKU/giphy.gif", votes: 7)
+post2.save
+post3 = Post.new(title: "Superman", gif: "https://media.giphy.com/media/R8MIGe47XWx68/giphy.gif", votes: 4)
+post3.save
+post4 = Post.new(title: "Cloud", gif: "https://media.giphy.com/media/Vvv5B8ZVpxf0s/giphy.gif", votes: 4)
+post4.save
+post5 = Post.new(title: "Cloud vs Sephirot", gif: "https://media.giphy.com/media/13S2ES2qJ7giwE/giphy.gif", votes: 7)
+post5.save
+post6 = Post.new(title: "Depor", gif: "https://media.giphy.com/media/lGhzVyHaLrlp6/giphy.gif", votes: 10)
+post6.save
+post7 = Post.new(title: "Joda", gif: "https://media.giphy.com/media/87I8pKmdcAKw8/giphy.gif", votes: 8)
+post7.save
+post8 = Post.new(title: "Sons of Anarchy", gif: "https://media.giphy.com/media/5MLNwYXJjvgTm/giphy.gif", votes: 3)
+post8.save
+post9 = Post.new(title: "Phelps", gif: "https://media.giphy.com/media/ETrDsCZQ3g3v2/giphy.gif", votes: 12)
+post9.save
+post10 = Post.new(title: "Jordan", gif: "https://media.giphy.com/media/pYvP6Bf0Uhtm0/giphy.gif", votes: 4)
+post10.save
+post11 = Post.new(title: "The Departed I", gif: "https://media.giphy.com/media/11BVidLzhKLgly/giphy.gif", votes: 7)
+post11.save
+post12 = Post.new(title: "The Departed II", gif: "https://media.giphy.com/media/mzPRxmvDsAW6A/giphy.gif", votes: 2)
+post12.save
+post13 = Post.new(title: "Joey", gif: "https://media.giphy.com/media/eHQQGL3dGOyQM/giphy.gif", votes: 8)
+post13.save
+post14 = Post.new(title: "Joey II", gif: "https://media.giphy.com/media/EU1obAC38GuWI/giphy.gif", votes: 4)
+post14.save
+post15 = Post.new(title: "Homer", gif: "https://media.giphy.com/media/xTk9ZTIiL8snKx84pi/giphy.gif", votes: 7)
+post15.save
+post16 = Post.new(title: "Harley Quinn", gif: "https://media.giphy.com/media/ujBfE6zDRAcYU/giphy.gif", votes: 11)
+post16.save
+post17 = Post.new(title: "Zizou", gif: "https://media0.giphy.com/media/8N5MCj3Mw4RJC/200.gif", votes: 10)
+post17.save
+post18 = Post.new(title: "William Wallace", gif: "https://media2.giphy.com/media/gwfatPkyl6WnC/200.gif", votes: 2)
+post18.save
+post19 = Post.new(title: "Homer Beer", gif: "https://media.giphy.com/media/xy6d8j1gO7XuE/giphy.gif", votes: 3)
+post19.save
+post20 = Post.new(title: "The Karate Kid", gif: "https://media.giphy.com/media/Lfz9qnyvCSeVG/giphy.gif", votes: 10)
+post20.save
+puts(" => Posts created √")
